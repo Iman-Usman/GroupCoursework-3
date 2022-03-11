0e20a92aab0dda54b169bbe9b7a6c3be0f50b6d4
Iman Usman <imanusman5053@yahoo.com>
2022-03-11 12:07:40 +0400

    done

---
+++ products.js
@@ -0,0 +1,12 @@
+let products=[
+    {
+    "id":1001,
+    "title":"Cat Food, 25lb bag",
+    "description": "A 25 pound bag of <em>irrestible</em> organic goodness for your cat.",
+    "price": 2000,
+    "image": "images/catfood.jpg",
+    "availableInventory": 10,
+    "rating": 1
+},
+]
+
